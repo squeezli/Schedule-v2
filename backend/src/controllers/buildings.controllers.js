48f54c6ab33d04830e1e389c428c7df6a7258883
Egor Iunin <egor-iunin@mail.ru>
2023-02-02 14:59:50 +0700

    update

--- backend/src/controllers/buildings.controllers.js
+++ backend/src/controllers/buildings.controllers.js
@@ -1,10 +1,3 @@
-const { sequelize } = require('sequelize')
-const User = require('../models/users.models.js')
-const bcrypt = require('bcrypt')
-const dotenv = require(dotenv)
-
-dotenv.config()
-
 exports.show = async (req, res) => {
     try {
 
@@ -15,19 +8,6 @@ exports.show = async (req, res) => {
 
 exports.create = async (req, res) => {
     try {
-        const { login, password, email } = req.body
-
-        const candidate = User.findAll({ login: login })
-        if (candidate) { return res.status(400).json({ message: `Пользователь с таким логином: ${login}, уже зарегистрирован.` }) }
-        
-        const hashPassword = bcrypt(password, process.env.SALT_ROUNDS)
-
-        const user = await User.create({
-            login, password, email
-        })
-
-
-        return res.status(201).json({message:`Пользователь зарегистрирован`})
 
     } catch (error) {
         return res.status(400).json({message:`Что то пошло не так, попробуйте снова`})
--- backend/src/controllers/users.controllers.js
+++ backend/src/controllers/users.controllers.js
@@ -1,3 +1,9 @@
+const { sequelize } = require('sequelize')
+const User = require('../models/users.models.js')
+const bcrypt = require('bcrypt')
+const dotenv = require(dotenv)
+dotenv.config()
+
 exports.show = async (req, res) => {
     try {
 
@@ -8,7 +14,19 @@ exports.show = async (req, res) => {
 
 exports.create = async (req, res) => {
     try {
+        const { login, password, email } = req.body
 
+        const candidate = User.findAll({ login: login })
+        if (candidate) { return res.status(400).json({ message: `Пользователь с таким логином: ${login}, уже зарегистрирован.` }) }
+
+        const hashPassword = bcrypt(password, process.env.SALT_ROUNDS)
+
+        const user = await User.create({
+            login, password, email
+        })
+
+
+        return res.status(201).json({ message: `Пользователь зарегистрирован` })
     } catch (error) {
 
     }
