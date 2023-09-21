141362347447e170e0b423fa8a42c5eda66a08a4
squeezli <85148436+squeezli@users.noreply.github.com>
2023-09-21 21:35:05 +0700

    Update teachers.jsx

--- frontend/src/pages/teacher/teachers.jsx
+++ frontend/src/pages/teacher/teachers.jsx
@@ -15,15 +15,9 @@ export const Teachers = () => {
 
     const [valueTeacher, setValueTeacher] = React.useState(null);
 
-
-    // const teachers = ['Черничко И.С.', 'Пархатская А.М.', 'Лиготина Ж.В.']
-
-
     const fetchClassrooms = React.useCallback(async () => {
         try {
             const fetched = await request(`/api/user/list`, 'GET', null);
-
-            console.log(fetched)
 
             setTeachers([...teachers, ...fetched.map(classroom => [classroom.teacher])])
 
@@ -34,28 +28,11 @@ export const Teachers = () => {
         fetchClassrooms();
     }, []);
 
-
     if (loading) return <p>Loading...</p>;
-
 
     return (
         <>
-
-
             <h1>Расписание преподавателей</h1>
-            {/* <Autocomplete
-                value={teacherValue}
-                onChange={(event, newValue) => {
-                    setTeacherValue(newValue);
-                    console.log('classroomValue: ', teacherValue)
-                }}
-                disablePortal
-                id="combo-box-demo"
-                options={teachers.map(items => items)}
-                sx={{ width: 300 }}
-                renderInput={(params) => <TextField {...params} label="Преподаватель" />}
-            /> */}
-
 
             <SelectorItems label='Преподаватель' items={teachers} setValue={setValueTeacher} value={valueTeacher} />
 
