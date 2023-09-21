da0b481486d19fb988c83f210bb0a9e4bb6c232e
squeezli <85148436+squeezli@users.noreply.github.com>
2023-09-21 21:36:41 +0700

    Update group.jsx

--- frontend/src/pages/group/group.jsx
+++ frontend/src/pages/group/group.jsx
@@ -9,45 +9,27 @@ export const Group = () => {
 
     const id = useParams().id;
 
-    // const startDate = moment().startOf('week').format('YYYY-MM-DD');
-    // const endDate = moment().endOf('week').format('YYYY-MM-DD');
-
-    // console.log(id)
-
     const { request, loading } = useHttp();
     const [schedule, setSchedule] = React.useState([]);
 
     const fetchSchedule = React.useCallback(async () => {
         try {
-
             const fetched = await request(`/api/schedule/group/${id}`, 'GET');
-            
-
             setSchedule([...schedule, ...fetched.map(group=>group)])
-           
-
-        } catch (error) {
-            console.log(error);
-        
-         }
+        } catch (error) {}
     }, [request]);
 
     React.useEffect(() => {
         fetchSchedule();
     }, []);
 
-
     if (loading) return <p>Loading...</p>;
     if (!schedule.length) return <p>Loading...</p>;
-
-
 
     return (
         <>
             <h1>Расписание {id} группы</h1>
-            {/* {console.log('dasdas  ',schedule)} */}
             <Schedule name={id} schedule={schedule}/>
-
         </>
     );
 }
