cae84084c294e9ba4c368bc7376c0248e5bbce4a
squeezli <85148436+squeezli@users.noreply.github.com>
2023-09-21 21:43:06 +0700

    Update classroom.jsx

--- frontend/src/pages/classroom/classroom.jsx
+++ frontend/src/pages/classroom/classroom.jsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import { useHttp } from "../../hooks/http.hook";
 import { useNavigate, useParams } from 'react-router-dom'
 import { Schedule } from '../../components/schedule/schedule';
-import moment from 'moment';
 
 export const Classroom = () => {
     
